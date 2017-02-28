1c0e16bcd6dc8e134d91b8a966b49b4766bf22a6
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2017-02-28 21:52:48 +0100

    * Added libProgressBar to .gitignore * Now runs to be processed must be specified in paths.txt

---
+++ README.md
@@ -0,0 +1,3 @@
+## NOTES ##
+
+* clone with `--recursive`
--- main.cxx
+++ main.cxx
@@ -1,7 +1,7 @@
 // test main for GERDA::DataReader class
 //
 // Author: Luigi Pertoldi (luigi.pertoldi@pd.infn.it)
-// Created: 01\02/2017
+// Created: 01/02/2017
 
 #include <iostream>
 #include <fstream>
@@ -14,27 +14,62 @@
 #include "TFile.h"
 
 int main( int argc, char** argv ) {
-
+    
+    // copy arguments
     std::vector<std::string> args(argc);
     for ( int i = 0; i < argc; i++ ) args[i] = argv[i];
-
+    
+    // set verbose
     bool verbose = false;
-    if ( argc > 1 and args[1] == "--verbose" ) verbose = true;
+    if ( std::find( args.begin(), args.end(), "--verbose") != args.end() ) verbose = true;
+    
+    /* NOT WORKING
+    // retrieve run IDs
+    std::vector<int> runsToProcess;
+    std::string tmp;
+    for ( auto& it : args ) {
+        if ( it.front() == '{' and it.back() == '}' ) {
+            for ( int i = 1; i < it.size()-1; i++ ) {
+                if ( it[i] != ',' ) tmp.append(it[i]);
+                if ( it[i] == ',' ) {
+                    if ( tmp.find('-') != std::string::npos ) {
+                        std::string tmp1, tmp2;
+                        tmp1.append(tmp[0]);
+                        tmp1.append(tmp[1]);
+                        tmp2.append(tmp[3]);
+                        tmp2.append(tmp[4]);
+                        for ( int j = std::stoi(tmp1); j <= std::stoi(tmp2); j++ ) {
+                            runsToProcess.push_back(j);
+                        }
+                        tmp1.clear();
+                        tmp2.clear();
+                    }
+                    else {
+                        runsToProcess.push_back(std::stoi(tmp));
+                        tmp.clear();
+                    }
+                }
+            }
+        }
+
+        else { std::cout << "bad input! See --help"<< std::endl; return -1; }
+    }
+    for ( auto& i : runsToProcess ) std::cout << i << " ";
+    */
 
     std::ifstream input("paths.txt");
     if ( !input.is_open() ) { std::cout << "File with paths not found! Aborting...\n"; return 0; }
     
     std::string metapath, datapath, configpath;
     input >> metapath >> datapath >> configpath;
+
+    std::vector<int> runsToProcess;
+    int value;
+    while ( input >> value ) runsToProcess.push_back(value);
     
     GERDA::DataReader reader( metapath, datapath, configpath);
-
-    reader.LoadRun(53, verbose);
     
-    std::cout << "Getting tree...\n" << std::flush;
-    auto chain = reader.GetTreeFromRun(53);
-    std::cout << "Getting number of entries...\n" << std::flush;
-    std::cout << chain->GetEntries() << std::endl;
+    for ( auto& it : runsToProcess ) reader.LoadRun(it, verbose);
 
     std::vector<TH1D> energy;
     energy = reader.GetEnergyHist();

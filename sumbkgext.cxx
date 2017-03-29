9dd63d32aab28ea0f95e387436c84639cf4194b0
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2017-03-29 10:32:55 +0200

    added K40_onFiberShroud

--- sumbkgext.cxx
+++ sumbkgext.cxx
@@ -31,8 +31,10 @@ int main( int argc, char** argv ) {
     std::vector<std::string> args(argc);
     for ( int i = 0; i < argc; ++i ) args[i] = argv[i];
     
-    //bool verbose = false;
-    //if ( std::find(args.begin(), args.end(), "--verbose"  ) != args.end() ) verbose = true; 
+    std::string phys;
+    if      ( std::find(args.begin(), args.end(), "--K42" ) != args.end() ) phys = "K42";
+    else if ( std::find(args.begin(), args.end(), "--K40" ) != args.end() ) phys = "K40";
+    else    { std::cout << "Specify --K42 or --K40!\n"; return -1; }
 // ----------------------------------------------------------------------------------------------------------
     
     // infos about runs
@@ -40,11 +42,19 @@ int main( int argc, char** argv ) {
     // detector types
     GERDA::DetectorSet set("GELATIO");
 
-    long int Ngen = 5E09;
-    // LAr mass [kg]
-    double M = 24563.385;
+    long int Ngen;
+    double M;
+    if ( phys == "K42" ) {
+        Ngen = 5E09;
+        M = 24563.385; // LAr mass [kg]
+    }
+    if ( phys == "K40" ) {
+        Ngen = 1E08;
+        M = 1.3615078; // fiber's mass [kg]
+    }
 
-    // fiber volume = 1.2966741074531858 E60 mm3
+    // fiber volume = 1.2966741074531858 E03 cm3
+    // density 1.05 g/cm^3
 
     // get detectorStatusMap for selected runs
     auto dsm = reader.GetDetectorStatusMap();
@@ -67,7 +77,8 @@ int main( int argc, char** argv ) {
     std::vector<TH1F*> hist;
 
     // get original spectra
-    path = std::string(std::getenv("GERDACPTDIR")) + "/out/K42_homLAr_5E09.root";
+    if ( phys == "K42" ) path = std::string(std::getenv("GERDACPTDIR")) + "/out/K42_homLAr_5E09.root";
+    if ( phys == "K40" ) path = std::string(std::getenv("GERDACPTDIR")) + "/out/K40_onFiberShroud_1E08.root";
     TFile infile(path.c_str(), "READ"); 
     if (!infile.IsOpen()) { std::cerr << "Zombie infile!\n"; return -1; }
 
@@ -76,7 +87,8 @@ int main( int argc, char** argv ) {
         if (hist[i]->IsZombie()) { std::cout << "Zombie h" << std::to_string(i) << "!\n"; return -1; }
     }
     
-    path = std::string(std::getenv("GERDACPTDIR")) + "/out/sumMaGe_homLAr.root";
+    if ( phys == "K42" ) path = std::string(std::getenv("GERDACPTDIR")) + "/out/sumMaGe_homLAr.root";
+    if ( phys == "K40" ) path = std::string(std::getenv("GERDACPTDIR")) + "/out/sumMaGe_K40onFiberShroud.root";
     TFile outfile(path.c_str(), "RECREATE");
     for ( int i = 0; i < 40; ++i ) {
         hist[i]->Scale(totalTime[i]*M/Ngen);

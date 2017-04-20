e0b5713b925c1825b8fb32c33c021ba7b9c79ff1
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2017-04-20 19:21:21 +0200

    adjusting ranges

--- fit/Fit2nbbLV.cxx
+++ fit/Fit2nbbLV.cxx
@@ -25,33 +25,33 @@
 Fit2nbbLV::Fit2nbbLV(std::string name) : BCModel(name.c_str()), kUseRange(false) {
    
     // define parameters
-    /* [0] */  this->AddParameter("2nbb",             200, 230);
-    /* [1] */  this->AddParameter("2nbbLV",           0, 2E-03);
-    /* [2] */  this->AddParameter("K42homLAr",        2E-04, 3E-04);
-    /* [3] */  this->AddParameter("K40fibers",        0, 1E-00);
-    /* [4] */  this->AddParameter("Bi212Tl208fibers", 0, 0.02);
-    /* [5] */  this->AddParameter("Pb214Bi214fibers", 0, 1E-01);
-    /* [6] */  this->AddParameter("alphaBEGe",        0, 2000);
-    /* [7] */  this->AddParameter("alphaCOAX",        0, 3500);
-    /* [8] */  this->AddParameter("K42nPlusBEGe",     0, 5E-04);
-    /* [9] */  this->AddParameter("K42nPlusCOAX",     0, 2E-03);
-    /* [10] */ this->AddParameter("K42pPlusBEGe",     0, 5E-01);
-    /* [11] */ this->AddParameter("K42pPlusCOAX",     0, 1E-03);
-    /* [12] */ this->AddParameter("Ac228holder",      0, 5E-04);
-    /* [13] */ this->AddParameter("Co60holder",       0, 5E-04);
-    /* [14] */ this->AddParameter("K40holder",        0, 5E-02);
-    /* [15] */ this->AddParameter("Bi212Tl208holder", 0, 5E-04);
-    /* [16] */ this->AddParameter("Pb214Bi214holder", 0, 5E01);
-    /* [17] */ this->AddParameter("K40cable",         0, 1E00);
-    /* [18] */ this->AddParameter("Bi212Tl208cables", 0, 5E-02);
-    /* [19] */ this->AddParameter("Pb214Bi214cables", 0, 1E03);
-    /* [20] */ this->AddParameter("K40minishroud",    0, 2E-01);
-    /* [21] */ this->AddParameter("Pa234minishroud",  0, 2E-01);
-    /* [22] */ this->AddParameter("Bi207minishroud",  0, 5E-03);
-    /* [23] */ this->AddParameter("Bi207cables",      0, 5E-02);
-    /* [24] */ this->AddParameter("Bi207holder",      0, 5E-02);
-    /* [25] */ this->AddParameter("Pb214Bi214minishroud", 0, 5E-01);
-    /* [26] */ this->AddParameter("K42minishroudsurface",  0, 5E-03);
+    /* [0] */  this->AddParameter("2nbb",                 2E02, 2.3E02);
+    /* [1] */  this->AddParameter("2nbbLV",               0, 2E-03);
+    /* [2] */  this->AddParameter("K42homLAr",            1.5E-04, 2.5E-04);
+    /* [3] */  this->AddParameter("K40fibers",            0, 1E-00);
+    /* [4] */  this->AddParameter("Bi212Tl208fibers",     0, 0.02);
+    /* [5] */  this->AddParameter("Pb214Bi214fibers",     0, 1E-01);
+    /* [6] */  this->AddParameter("alphaBEGe",            8E02, 1.5E03);
+    /* [7] */  this->AddParameter("alphaCOAX",            2E03, 3E03);
+    /* [8] */  this->AddParameter("K42nPlusBEGe",         0, 2E-04);
+    /* [9] */  this->AddParameter("K42nPlusCOAX",         0, 3E-04);
+    /* [10] */ this->AddParameter("K42pPlusBEGe",         0, 7E-01);
+    /* [11] */ this->AddParameter("K42pPlusCOAX",         0, 1E-03);
+    /* [12] */ this->AddParameter("Ac228holder",          0, 7E-04);
+    /* [13] */ this->AddParameter("Co60holder",           0, 3E-04);
+    /* [14] */ this->AddParameter("K40holder",            0, 2E-02);
+    /* [15] */ this->AddParameter("Bi212Tl208holder",     0, 7E-04);
+    /* [16] */ this->AddParameter("Pb214Bi214holder",     0, 3E01);
+    /* [17] */ this->AddParameter("K40cable",             0, 1E00);
+    /* [18] */ this->AddParameter("Bi212Tl208cables",     0, 5E-02);
+    /* [19] */ this->AddParameter("Pb214Bi214cables",     0, 1E03);
+    /* [20] */ this->AddParameter("K40minishroud",        0, 2E-01);
+    /* [21] */ this->AddParameter("Pa234minishroud",      0, 2E-01);
+    /* [22] */ this->AddParameter("Bi207minishroud",      0, 5E-03);
+    /* [23] */ this->AddParameter("Bi207cables",          0, 7E-02);
+    /* [24] */ this->AddParameter("Bi207holder",          0, 5E-02);
+    /* [25] */ this->AddParameter("Pb214Bi214minishroud", 0, 3E03);
+    /* [26] */ this->AddParameter("K42minishroudsurface", 0, 7E-04);
 
     // TODO: add new parameters here
     //// LEGEND
--- fit/runfit.cxx
+++ fit/runfit.cxx
@@ -38,7 +38,7 @@ int main( int argc, char** argv ) {
 /////////////////////////////////////////////
     const int rangeUp = 5300;  // [keV]
     const int rangeDown = 570; // [keV] above 39Ar Q-value
-    BCEngineMCMC::Precision level(BCEngineMCMC::kMedium);
+    BCEngineMCMC::Precision level(BCEngineMCMC::kLow);
 /////////////////////////////////////////////
 
     auto c_str = [](std::string s) { return s.c_str(); };
@@ -260,11 +260,11 @@ int main( int argc, char** argv ) {
     BCLog::SetLogLevelScreen(BCLog::detail);
     model.FindMode(model.GetBestFitParameters());
     BCLog::SetLogLevelScreen(BCLog::summary);
-
+/*
     std::cout << std::endl;
     double pvalue = GetPValue(model, level);
     std::cout << "Summary : pValue = " << pvalue << std::endl;
-
+*/
     // OUTPUT
     // print results of the analysis into a text file
     model.PrintResults(c_str(path + "out/Fit2nbbLV_results.txt"));
